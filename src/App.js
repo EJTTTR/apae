b1d86a301a247b4c263c7580363b947e5fd4e2b2
EJTTTR <134746417+EJTTTR@users.noreply.github.com>
2024-11-03 05:23:10 -0400

    gi

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MindCareLanding from '@/components/MindCareLanding'
+// import DashboardView from '@/components/DashboardView'
+
+export default function Home() {
+  return (
+    <main>
+      <MindCareLanding />
+      {/* <DashboardView /> */}
+    </main>
+  )
+}
